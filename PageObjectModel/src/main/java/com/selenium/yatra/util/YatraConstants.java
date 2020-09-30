1930171d795adcf3d361c24ad3b744e06486511b
kamaljeetsn <kamaljeetsn@gmail.com>
2020-09-30 23:19:56 +0530

    second commit

--- PageObjectModel/src/main/java/com/selenium/yatra/util/YatraConstants.java
+++ PageObjectModel/src/main/java/com/selenium/yatra/util/YatraConstants.java
@@ -2,7 +2,7 @@ package com.selenium.yatra.util;
 
 public class YatraConstants {
 	
-	public static final boolean GRID_EXECUTION = true;
+	public static final boolean GRID_EXECUTION = false;
 	
 	public static final String CHROME_DRIVER_EXE = "D:\\Trainings\\Software\\Drivers\\chromedriver.exe";
 
